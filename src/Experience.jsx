01710e71a891618bab2efe67fa1b9281fa9caaee
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2024-04-15 00:33:30 -0600

    ecctrls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,33 +1,44 @@
 import { Canvas } from "@react-three/fiber";
-import Player from "./components/Player";
 import { PointerLockControls, KeyboardControls, Sky } from "@react-three/drei";
+import Ecctrl, { EcctrlJoystick } from "ecctrl";
 import Map from "./components/Map";
 import { Physics } from "@react-three/rapier";
 import Lights from "./components/Lights";
 
 export default function Experience() {
+  const keyboardMap = [
+    { name: "forward", keys: ["ArrowUp", "w", "W"] },
+    { name: "backward", keys: ["ArrowDown", "s", "S"] },
+    { name: "leftward", keys: ["ArrowLeft", "a", "A"] },
+    { name: "rightward", keys: ["ArrowRight", "d", "D"] },
+    { name: "jump", keys: ["Space"] },
+    { name: "run", keys: ["Shift"] },
+  ];
+
   return (
     <>
-      <KeyboardControls
-        map={[
-          { name: "forward", keys: ["ArrowUp", "w", "W"] },
-          { name: "backward", keys: ["ArrowDown", "s", "S"] },
-          { name: "left", keys: ["ArrowLeft", "a", "A"] },
-          { name: "right", keys: ["ArrowRight", "d", "D"] },
-          { name: "jump", keys: ["Space"] },
-        ]}
-      >
-        <Canvas shadows camera={{ fov: 45 }}>
-          <Sky sunPosition={[100, 20, 100]} />
-          <Lights />
+      <EcctrlJoystick />
+      <Canvas shadows camera={{ fov: 45 }}>
+        <Sky sunPosition={[100, 20, 100]} />
+        <Lights />
 
-          <Physics gravity={[0, -30, 0]}>
-            <Map />
-            <Player />
-          </Physics>
-          <PointerLockControls />
-        </Canvas>
-      </KeyboardControls>
+        <Physics gravity={[0, -30, 0]}>
+          <Map />
+          <KeyboardControls map={keyboardMap}>
+            <Ecctrl
+              camInitDis={-0.01} // camera intial position
+              camMinDis={-0.01} // camera zoom in closest position
+              camFollowMult={100} // give any big number here, so the camera follows the character instantly
+              turnVelMultiplier={1} // Turning speed same as moving speed
+              turnSpeed={100} // give it big turning speed to prevent turning wait time
+              mode="CameraBasedMovement" // character's rotation will follow camera's rotation in this mode
+            >
+              <mesh />
+            </Ecctrl>
+          </KeyboardControls>
+        </Physics>
+        <PointerLockControls />
+      </Canvas>
     </>
   );
 }
